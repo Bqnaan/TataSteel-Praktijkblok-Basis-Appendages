4a8a6a3641c774abb3a5019fa87c1f406ac5dcf9
Bqnaan <sjoerdsteeman@gmail.com>
2022-07-06 11:50:06 +0200

    initial commit

---
+++ Assets/Scripts/Quiz scripts/QuestionsAndAnswers.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+[System.Serializable]
+public class QuestionsAndAnswers
+{
+    public string Question;
+    public Sprite image;
+    public string[] Answers;
+    public int CorrectAnswer;
+}
